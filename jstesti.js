6a2aac1f83902d02bfbb7fff050bda382c8362ed
Ossi Weck <ossi.weck@student.careeria.fi>
2024-01-17 11:03:11 +0200

    ruokkis

---
+++ jstesti.js
@@ -0,0 +1,12 @@
+
+// Palvelut sivun näyttäminen
+function palvelut(){
+    let palvelut = `<h2>Palvelumme</h2>
+    
+            <p>Eläinlääkäri</p>
+            <p>Päivähoito</p>
+            <p>Yöhoito</p>
+    
+    `
+    document.getElementById("sisältö").innerHTML = palvelut
+}
